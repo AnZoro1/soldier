9f07cf8e785782b36e100e5bcea2ebc2eddefd20
AnzorKarto <beit06@mail.ru>
2022-11-01 16:05:26 +0300

    Сделал объект и пустые методы, 1 подзадача

---
+++ soldiers.js
@@ -0,0 +1,9 @@
+const soldier = {
+  name: 'John',
+  health: 10,
+  gun: { nameGun: 'kalash', cartridges: 30 },
+  supplies: 3,
+  shot: function () {},
+  recharge: function () {},
+  hurt: function () {},
+}
