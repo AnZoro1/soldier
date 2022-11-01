189495da46ab8bbdce46b3fb5c3cdddbf8621fcc
AnzorKarto <beit06@mail.ru>
2022-11-01 16:57:24 +0300

    выполнил 3 подзадачу

--- soldiers.js
+++ soldiers.js
@@ -4,19 +4,27 @@ const soldier = {
   gun: { nameGun: 'kalash', cartridges: 30 },
   supplies: 3,
   shot: function () {
-    this.gun.cartridges--
-    if (this.gun.cartridges == 0) {
-      return 'обойма пуста. Перезаредитесь'
+    if (this.gun.cartridges === 0) {
+      console.log('обойма пуста. Перезаредитесь')
+      soldier.recharge()
     }
+    this.gun.cartridges--
+    console.log(soldier.gun.cartridges)
   },
+
   recharge: function () {
     this.gun.cartridges = 30
     this.supplies--
+    if (this.supplies === 0) {
+      console.log('не осталось припасов')
+    }
+    console.log(this.supplies)
   },
   hurt: function () {
     this.health--
     if (this.health == 0) {
       console.log('Ты проиграл')
     }
+    console.log(this.health)
   },
 }
